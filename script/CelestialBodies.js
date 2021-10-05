e6a620705f5874e5f3996cce76061e5461a49cb9
Kathrin Janowski <science@kathrinjanowski.com>
2021-10-05 21:34:14 +0200

    labels

--- script/CelestialBodies.js
+++ script/CelestialBodies.js
@@ -114,7 +114,7 @@ function drawCelestialBody(ctx, originX, originY, centerX, centerY, celestial)
 	ctx.fillStyle = "#FFFFFF";
 	fontY = centerY + celestial.radius + 20;
 	var fontWidth = ctx.measureText(celestial.name[culture])*0.5;
-	ctx.shadowBlur=10;
+	ctx.shadowBlur=5;
 	ctx.fillText(celestial.name[culture], centerX-fontWidth, fontY);
 	ctx.shadowBlur=0;
 	
@@ -271,7 +271,7 @@ function updateStarSystem(){
 	ctx.fillStyle = "#FFFFFF";
 	fontY = originY + star.radius + 20;
 	var fontWidth = ctx.measureText(star.name[culture])*0.5;
-	ctx.shadowBlur=10;
+	ctx.shadowBlur=5;
 	ctx.fillText(star.name[culture], originX-fontWidth, fontY);
 	ctx.shadowBlur=0;
 
